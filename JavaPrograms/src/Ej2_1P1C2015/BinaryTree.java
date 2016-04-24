83c92cb9827068f41a1dcf0f52498e58df4041f6
epord <pedro.balaguer@hotmail.com>
2016-04-24 15:44:55 -0300

    Added Exam Exercise (Ej2 1P1C2015)
    The exercise was asking to search all subtrees coincidences between two trees.

---
+++ JavaPrograms/src/Ej2_1P1C2015/BinaryTree.java
@@ -0,0 +1,36 @@
+package Ej2_1P1C2015;
+
+import java.util.Objects;
+
+/**
+ * Created by Pedro on 23/4/2016.
+ */ public class BinaryTree<T> {
+    public T value;
+    public BinaryTree<T> left, right;
+
+    public BinaryTree(T value, BinaryTree<T> left, BinaryTree<T> right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+
+    }
+
+    public String toString() {
+        return value.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BinaryTree<?> that = (BinaryTree<?>) o;
+        return Objects.equals(value, that.value) &&
+                Objects.equals(left, that.left) &&
+                Objects.equals(right, that.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
