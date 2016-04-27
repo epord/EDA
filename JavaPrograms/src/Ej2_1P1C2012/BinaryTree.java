564c9c60ef3b1ff1b83eef1e28bb3a40369bae79
epord <pedro.balaguer@hotmail.com>
2016-04-27 01:11:03 -0300

    Added Exam Exercise (ej2 1P1C2012)
    The exercise was asking to return a sub tree that have all branches ending with a determinated value.

---
+++ JavaPrograms/src/Ej2_1P1C2012/BinaryTree.java
@@ -0,0 +1,60 @@
+package Ej2_1P1C2012;
+
+/**
+ * Created by Pedro on 27/4/2016.
+ */
+public class BinaryTree<T> {
+    private T value;
+    private BinaryTree<T> left, right;
+
+    public BinaryTree(T value, BinaryTree<T> left, BinaryTree<T> right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public BinaryTree<T> getLeft() {
+        return left;
+    }
+
+    public BinaryTree<T> getRight() {
+        return right;
+    }
+
+    public static <T> BinaryTree<T> spanning(BinaryTree<T> tree, T value) {
+        if (tree == null) {
+            return null;
+        }
+        BinaryTree<T> auxL = null;
+        BinaryTree<T> auxR = null;
+        if (tree.value.equals(value)) {
+            if (tree.left != null && tree.left.value.equals(value)) {
+                auxL = spanning(tree.left, value);
+            }
+            if (tree.right != null && tree.right.value.equals(value)) {
+                auxR = spanning(tree.right, value);
+            }
+            return new BinaryTree<>(value, auxL, auxR);
+        }
+        auxL = spanning(tree.left, value);
+        auxR = spanning(tree.right, value);
+        if (auxL == null && auxR == null) {
+            return null;
+        }
+        return new BinaryTree<>(tree.value, auxL, auxR);
+    }
+
+    public void printPreOrder() {
+        System.out.printf("%s ", value.toString());
+        if (left != null) {
+            left.printPreOrder();
+        }
+        if (right != null) {
+            right.printPreOrder();
+        }
+    }
+}
---
+++ JavaPrograms/src/Ej2_1P1C2012/Test.java
@@ -0,0 +1,21 @@
+package Ej2_1P1C2012;
+
+/**
+ * Created by Pedro on 27/4/2016.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        BinaryTree<String> bD1 = new BinaryTree<>("D", null, null);
+        BinaryTree<String> bD2 = new BinaryTree<>("D", null, null);
+        BinaryTree<String> bE = new BinaryTree<>("E", null, null);
+        BinaryTree<String> bB2 = new BinaryTree<>("B", bD2, bE);
+        BinaryTree<String> bB1 = new BinaryTree<>("B", bD1, bB2);
+        BinaryTree<String> bB3 = new BinaryTree<>("B", null, null);
+        BinaryTree<String> bC = new BinaryTree<>("C", null, bB3);
+        BinaryTree<String> bA = new BinaryTree<>("A", bB1, bC);
+
+        BinaryTree.spanning(bA, "D").printPreOrder();
+    }
+
+}
